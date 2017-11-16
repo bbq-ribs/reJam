03c3c4535413c41358a08d6dc9eeca0d0cff518c
Brandon Hoffman <brandonhoffman@gmail.com>
2017-11-16 00:32:46 -0800

    initial commit to Heroku

--- secret_sauce/app.js
+++ secret_sauce/app.js
@@ -13,7 +13,7 @@ var querystring = require("querystring");
 var cookieParser = require("cookie-parser");
 
 var client_id = "e45ed3cdf4e4442a8bc1fc79a1c2fa0f"; // Your client id
-var client_secret = ""; // Your secret
+var client_secret = "814bd2308429425a91d01aaa3e6d4d4c"; // Your secret
 var redirect_uri = "http://localhost:8888/callback"; // Your redirect uri
 
 /**
